0f474986f347d0efaf114304fec87a71be647cac
Ronald Krings Revollo <202202861@est.umss.edu>
2026-06-18 00:46:39 -0400

    agregados archivos haskell

---
+++ HASKELL/enteros/enteros_100mil.hs
@@ -0,0 +1,30 @@
+import System.IO (readFile)
+import Control.Exception (evaluate)
+import Control.DeepSeq (rnf)
+
+-- Algoritmo Quicksort
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (p:xs) = quicksort menores ++ [p] ++ quicksort mayores
+  where
+    menores = filter (< p) xs
+    mayores = filter (>= p) xs
+
+main :: IO ()
+main = do
+  -- 1. Leemos todo el contenido del archivo txt
+  contenido <- readFile "enteros_100mil.txt"
+  
+  -- 2. Convertimos el texto en una lista de enteros
+  -- 'words' separa el texto por espacios o saltos de línea
+  -- 'map read' convierte cada fragmento de texto en un número Int
+  let listaNumeros = map read (words contenido) :: [Int]
+  
+  putStrLn "Iniciando ordenamiento..."
+  
+  -- 3. Forzamos la evaluación del algoritmo para medir el tiempo real
+  listaOrdenada <- evaluate (quicksort listaNumeros)
+  --'rnf' (reduce to normal form) para asegurarnos que Haskell realmente ordene todo en este preciso momento.
+  evaluate (rnf listaOrdenada)
+  
+  putStrLn "Ordenamiento completado."
---
+++ HASKELL/fechas/fechas_500mil.hs
@@ -0,0 +1,31 @@
+import System.IO (readFile)
+import Control.Exception (evaluate)
+import Control.DeepSeq (rnf)
+
+-- Algoritmo Quicksort
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (p:xs) = quicksort menores ++ [p] ++ quicksort mayores
+  where
+    menores = filter (< p) xs
+    mayores = filter (>= p) xs
+
+main :: IO ()
+main = do
+  -- 1. Leemos todo el contenido del archivo txt
+  contenido <- readFile "fechas_500mil.txt"
+  
+  -- 2. Convertimos el texto en una lista de enteros
+  -- 'words' separa el texto por espacios o saltos de línea
+  -- 'map read' convierte cada fragmento de texto en una cadena
+  let listaPalabras = words contenido
+  
+  putStrLn "Iniciando ordenamiento..."
+  
+  -- 3. Forzamos la evaluación del algoritmo para medir el tiempo real
+  listaOrdenada <- evaluate (quicksort listaPalabras)
+  --'rnf' (reduce to normal form) para asegurarnos que Haskell realmente ordene todo en este preciso momento.
+  evaluate (rnf listaOrdenada)
+  
+  putStrLn "Ordenamiento completado."
+  print (take 10 listaOrdenada)
